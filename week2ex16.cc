2671cb2817c85e9e9b8882bc4c6c2d47f6d1fabd
SilverLuca <91182564+SilverLuca@users.noreply.github.com>
2021-09-27 22:38:04 +0200

    has magic number ;-;

--- week2ex16.cc
+++ week2ex16.cc
@@ -1,27 +1,37 @@
 #include <iostream>
-#include <math.h>
 
 using namespace std;
 
 int main(int argc, char *argv[])
 {
-    size_t radix = stoul(argv[1]);
-    size_t value = stoul(argv[2]);
-    size_t idx = 0;
-    size_t digits[] = {};
+    size_t radix = stoul(argv[1]);			// initialize first input as radix
+    size_t value = stoul(argv[2]);			// initialize second input as value
+    size_t idx = 0;					// initialize idx to loop back and forth
+    size_t digits[] = {};				// initialize digits array
 
-    cout << value << ", displayed using radix " << radix << " is: ";
+    cout << value << ", displayed using radix "
+	<< radix << " is: ";				// prints the first part of the sentence
 
-    while(value > radix)
+    while(value > radix)				// loops until no more new digits are needed
     {
-	digits[idx] = value % radix;
-	value =  value / radix;
-	++idx;
+	digits[idx] = value % radix;			// fills digits array with the remainder
+	value =  value / radix;				// getting ready for the next digit (floors the value)
+	++idx;						// increases index
     }
 
-    digits[idx] = value;
+    digits[idx] = value;				// fills the last required digit
 
-    for (idx = idx+1; idx--;)
-        cout << digits[idx];
-    cout << '\n';
+    for (idx = idx+1; idx--;)				// loops back over the entries in the array
+        if(digits[idx] > 9)				// check if a number should convert to a letter
+	{
+	    int letter = digits[idx] + 87;		// ewww magic number
+	    cout << (char) letter;			// prints the letter
+	}
+	else
+	{
+            cout << digits[idx];			// prints the number
+	}
+
+    cout << '\n';					// ends the line
 }
+
