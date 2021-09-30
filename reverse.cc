76b94a27da4c73d60caf166c18a0c3f4d38b7f0c
SilverLuca <91182564+SilverLuca@users.noreply.github.com>
2021-09-30 15:56:46 +0200

    add comments

--- reverse.cc
+++ reverse.cc
@@ -9,16 +9,16 @@ int main()
 
     while(true)					// allows to input continuously
     {
-        cout << "? ";				// 
-        getline (cin, text);			//
-        size_t count = text.length();		//
+        cout << "? ";				// gives the prompt to write
+        getline (cin, text);			// puts the input into the text string
+        size_t count = text.length();		// counts the amount of characters written
 
-        if(count == 0)				//
-            break;				//
+        if(count == 0)				// checks if promt is empty
+            break;				// yes? stop the program
 
-	for(size_t idx = count ; idx--; )	//
-	    cout << text.at(idx);		//
+	for(size_t idx = count ; idx--; )	// loop to work backwards through the string
+	    cout << text.at(idx);		// prints the text in reverse
 
-        cout << '\n';				//
+        cout << '\n';				// end the line
     }
 }
