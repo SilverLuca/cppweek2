62b50e80036420d2292b18ecb5948f96ffa4e878
SilverLuca <91182564+SilverLuca@users.noreply.github.com>
2021-10-04 19:34:35 +0200

    space

--- week2ex9.cc
+++ week2ex9.cc
@@ -7,7 +7,7 @@ int main(int argc, char *argv[])
     // converts input to integer
     size_t const table_number = stoul(argv[1]);
 
-    for(size_t idx=1; idx != 11; ++idx)
+    for (size_t idx=1; idx != 11; ++idx)
 	// outputs disired text
 	cout << idx << " * " << table_number
 	<< " = " << table_number * idx << '\n';
